c067960db211db2ad2d662c61960b01dd4642724
Your Name <you@example.com>
2020-10-09 18:20:07 +0200

    improvement accessibily

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,24 @@
 //Header.js
-import React from 'react'
-import{Navbar, Nav} from 'react-bootstrap'
-import {Link} from 'gatsby'
+import React from "react"
+import { Navbar, Nav } from "react-bootstrap"
+import { Link } from "gatsby"
 
 export default props => (
   <Navbar bg="light" expand="lg">
     <Navbar.Brand href="/">Gatsby-Bootstrap</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+      <Nav className="mr-auto">
         <Nav.Link as={Link} to="/">
-          Home
+          <b className="text-dark">Home</b>
         </Nav.Link>
         <Nav.Link as={Link} to="/about">
-          About us
+          <b className="text-dark">About us</b>
         </Nav.Link>
         <Nav.Link as={Link} to="/contact">
-          Contact us
+          <b className="text-dark">Contact us</b>
         </Nav.Link>
-        </Nav>
+      </Nav>
     </Navbar.Collapse>
   </Navbar>
 )
