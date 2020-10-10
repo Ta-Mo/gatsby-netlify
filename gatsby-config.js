cc5239dd623ee7f2b0777897a07109f70a797ce8
Your Name <you@example.com>
2020-10-10 13:32:53 +0200

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,5 +36,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-react-helmet`,
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS Bootstrap Tutorial`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f8f9fa`,
+        theme_color: `#ffffff`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-offline`
   ],
 }
